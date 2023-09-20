7bc47fd44d62051ab3d28d1c63363698c9ef0403
xiabaike <xiabaike@163.com>
2023-09-20 10:49:33 +0800

    a

---
+++ src/main/java/com/xiabaike/interview23/BeiKe.java
@@ -0,0 +1,27 @@
+package com.xiabaike.interview23;
+
+public class BeiKe {
+
+    // head 头结点
+    // 判断有没有环
+    // [1,2,3,4,5]  5->3
+
+    // 依次把 链表 的各节点，放入 set，每次判断是否已经存在，如果存在则true
+//    while (head != null) {
+//        if (set.contains(head)) {
+//            return true;
+//        }
+//        set.add(head);
+//        head = head.next;
+//    }
+//    return false;
+//
+//    // 引入两个指针 fast、slow = head，每次 fast 走两步，slow 走一步
+//    fast ， slow = head
+//    while (fast != null && fase.next != null) {
+//        fast = fast.next.next;
+//        slow = slow.next;
+//    }
+
+
+}
